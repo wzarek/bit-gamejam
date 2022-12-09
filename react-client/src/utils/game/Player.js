d45613a34822584d0ec3cb24a4c84aca88db2f4c
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2022-12-10 00:30:54 +0100

    Implemented canvas boundries for player

--- react-client/src/utils/game/Player.js
+++ react-client/src/utils/game/Player.js
@@ -16,6 +16,9 @@ export default class Player {
     #y = 0
     #width = 30
     #height = 30
+    #speedX = 0
+    #speedY = 0
+    #maxSpeed = 5
 
     constructor(game, isCurrentPlayer) {
         this.#gameObject = game
@@ -23,18 +26,31 @@ export default class Player {
     }
 
     update(inputKeys) {
-        if (inputKeys.includes(this.#keys.up)) {
-            this.#y--
+        if (inputKeys.includes(this.#keys.left) || inputKeys.includes(this.#keys.right)) {
+            this.#x += this.#speedX
+            if (inputKeys.includes(this.#keys.left)) {
+                this.#speedX = -this.#maxSpeed
+            } else if (inputKeys.includes(this.#keys.right)) {
+                this.#speedX = this.#maxSpeed
+            }
+        } else{
+            this.#speedX = 0
         }
-        if (inputKeys.includes(this.#keys.down)) {
-            this.#y++
+
+        if (inputKeys.includes(this.#keys.up) || inputKeys.includes(this.#keys.down)) {
+            this.#y += this.#speedY
+            if (inputKeys.includes(this.#keys.up)) {
+                this.#speedY = -this.#maxSpeed
+            }
+            if (inputKeys.includes(this.#keys.down)) {
+                this.#speedY = this.#maxSpeed
+            }
         }
-        if (inputKeys.includes(this.#keys.left)) {
-            this.#x--
-        }
-        if (inputKeys.includes(this.#keys.right)) {
-            this.#x++
-        }
+
+        if (this.#x < 0) this.#x = 0
+        if (this.#y < 0) this.#y = 0
+        if (this.#x > this.#gameObject.width - this.#width) this.#x = this.#gameObject.width - this.#width
+        if (this.#y > this.#gameObject.height - this.#height) this.#y = this.#gameObject.height - this.#height
     }
 
     render(ctx) {
