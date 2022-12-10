271755be7bc019e330b13f100a99bfca13a4ccb4
Maciej Krawczyk <63869461+wzarek@users.noreply.github.com>
2022-12-10 09:59:05 +0100

    Handled players rendering

--- react-client/src/components/Lobby/Room.js
+++ react-client/src/components/Lobby/Room.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 const Room = (props) => {
+  const navigate = useNavigate()
+
   return (
-    <div>
+    <button className='bg-slate-500 mt-2 rounded-lg p-1' onClick={() => navigate(`/room/${props.name}`)}>
       {props.name}
-    </div>
+    </button>
   )
 }
 
